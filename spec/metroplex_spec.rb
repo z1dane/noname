cc4bec333586e397cac96e8d6995b1b117d02ee7
Kalmykov Kirill <slyplan@gmail.com>
2012-03-25 18:32:22 +0400

    configured rspec and some authentication tests

--- spec/metroplex_spec.rb
+++ spec/metroplex_spec.rb
@@ -8,9 +8,62 @@ end
 describe 'Metroplex' do
   include Rack::Test::Methods
   
-  it 'should work' do 
+  it 'should return content of index.html' do 
     get '/'
-    last_response.should be_ok
+    File.open(File.join(settings.root, 'public', 'index.html')) do |file|
+      last_response.should equal file.read
+    end
   end
 
+  describe 'in actions with sessions' do  
+
+    it 'should authenticate users' do
+      post '/login'
+      last_response.should be_ok
+    end
+
+    context 'when user unauthenticated' do
+
+      before do
+        User.create valid_user
+        session[:user_id] = nil
+      end
+      
+      it 'should not starts a session when login is incorrect' do
+        post '/login', :login => "invalid", :password => "user"
+        session[:user_id].should be_nil
+      end
+
+      it 'should not starts a session when password is incorrect' do
+        post '/login', :login => "user", :password => "123"
+        session[:user_id].should be_nil
+      end
+
+      it 'should start session when login and password is correct' do
+        post '/login', :login => "user", :password => "user"
+        session[:user_id].sould_not be_nil
+      end
+      
+    end
+
+    context 'when user authenticated' do
+
+      before do
+        @user = User.create valid_user
+        @user.authenticate
+      end
+
+      it 'should be authenticated' do
+        session[:user_id].should_not be_nil
+      end
+
+      it 'should be able to log out' do
+        get '/logout'
+        session[:user_id].should be_nil
+      end
+      
+    end
+    
+  end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,5 @@ require 'rack/test'
 set :environment, :test
 set :run, false
 set :raise_errors, true
+set :root, File.join(File.dirname(__FILE__), "..")
+
